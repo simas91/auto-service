fa60ba34702a28a8172efad2094a5eea16825256
simas <simas91@gmail.com>
2020-02-21 13:51:40 +0000

    added hikari datasource, flyaway db migration, vehicle dao

--- src/main/java/autoservice/controller/VehicleController.java
+++ src/main/java/autoservice/controller/VehicleController.java
@@ -1,34 +1,28 @@
 package autoservice.controller;
 
 import autoservice.domain.Vehicle;
+import autoservice.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("vehicle")
 public class VehicleController {
 
+    private final VehicleService vehicleService;
+
+    @Autowired
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
     @GetMapping
     public List<Vehicle> getAllVehicles() {
-        return List.of(
-                new Vehicle(UUID.randomUUID(),
-                        "FD60LWM",
-                        "Honda",
-                        "Civic",
-
-                        2011,
-                        91300),
-                new Vehicle(UUID.randomUUID(),
-                        "AE04SWJ",
-                        "Yamaha",
-                        "R1",
-                        2004,
-                        15233)
-        );
+        return vehicleService.getAllVehicles();
     }
 
 }
--- src/main/java/autoservice/service/VehicleService.java
+++ src/main/java/autoservice/service/VehicleService.java
@@ -1,4 +1,23 @@
 package autoservice.service;
 
+import autoservice.dao.VehicleDAO;
+import autoservice.domain.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VehicleService {
+
+    private final VehicleDAO vehicleDAO;
+
+    @Autowired
+    public VehicleService(VehicleDAO vehicleDAO) {
+        this.vehicleDAO = vehicleDAO;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return vehicleDAO.selectAllVehicles();
+    }
 }
